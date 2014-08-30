88d001858e76f3401d0e5a4b54911fdb6a19201a
Shane Osbourne <shane.osbourne8@gmail.com>
2014-08-30 17:49:44 +0100

    Add e2e spec for urls

--- test/items/post.js
+++ test/items/post.js
@@ -9,8 +9,8 @@ var post1 = multiline.stripIndent(function(){/*
 layout: post-test
 title: "Homepage"
 date: 2013-11-13
-categories: javascript, node js
-tags: code, jquery-ui, how to guide
+categories: javascript, node-js
+tags: code, jquery-ui, how-to-guide
 ---
 post1
  */});
@@ -21,7 +21,7 @@ describe("Creating a POST with maximum info", function(){
         var postItem = new Post("_posts/post1.md", post1);
         assert.isTrue(postItem instanceof Post);
     });
-    it("Has access to front matter", function() {
+    it("sets expected data", function() {
 
         var postItem = new Post("_posts/post1.md", post1);
 
@@ -33,9 +33,25 @@ describe("Creating a POST with maximum info", function(){
         assert.deepEqual(postItem.timestamp,    1384300800000,       "Adds Timestamp");
         assert.deepEqual(postItem.type,         "post",              "Adds Timestamp");
 
-        assert.deepEqual(postItem.categories,   ["javascript", "node js"], "Adds Categories");
-        assert.deepEqual(postItem.tags,   ["code", "jquery-ui", "how to guide"], "Adds tags");
+        assert.deepEqual(postItem.categories,   ["javascript", "node-js"], "Adds Categories");
+        assert.deepEqual(postItem.tags,   ["code", "jquery-ui", "how-to-guide"], "Adds tags");
 
         assert.isTrue(postItem.dateObj instanceof Date);
+    });
+    it("Uses Config", function() {
+
+        var postItem = new Post("_posts/post1.md", post1, {
+            postUrlFormat: "/blog/:categories/:title",
+            prettyUrls: true
+        });
+        assert.deepEqual(postItem.url, "/blog/javascript/node-js/post1", "Adds URL + categories");
+    });
+    it("Uses Config", function() {
+
+        var postItem = new Post("_posts/post1.md", post1, {
+            postUrlFormat: "/blog/:categories/:title",
+            prettyUrls: false
+        });
+        assert.deepEqual(postItem.url, "/blog/javascript/node-js/post1.html", "Adds URL + categories");
     });
 });
