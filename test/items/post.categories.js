120f47079f37b5afb92dbfadc3ace8a98e4e1763
Shane Osbourne <shane.osbourne8@gmail.com>
2014-09-08 22:31:12 +0100

    Override sep helper

--- test/items/post.categories.js
+++ test/items/post.categories.js
@@ -16,6 +16,24 @@ categories: javascript, node-js
 {#post.related}
 {title}
 {/post.related}
+
+{#post.categories}{.}{@sep},{/sep}{/post.categories}
+
+ */});
+var post1_alt = multiline.stripIndent(function(){/*
+---
+layout: post-test
+title: "Post 1"
+date: 2014-01-01
+categories: javascript
+---
+
+{#post.related}
+{title}
+{/post.related}
+
+{#post.categories}{.}{@sep},{/sep}{/post.categories}
+
  */});
 var post2 = multiline.stripIndent(function(){/*
 ---
@@ -37,7 +55,7 @@ describe("Creating a Post that knows about others in the same category", functio
         crossbow.populateCache("_layouts/post-test.html", "{#content /}");
     });
 
-    it("uses the filename as title if no title available in front-matter (e2e)", function() {
+    it("can list the categories with seperator", function() {
 
 
         var postItem  = crossbow.addPost("_posts/post1.md", post1);
@@ -46,7 +64,23 @@ describe("Creating a Post that knows about others in the same category", functio
         crossbow.compileOne(postItem, {}, function (err, out) {
             console.log(out.compiled);
             assert.include(out.compiled, "<p>Post 2</p>");
+            assert.include(out.compiled, "<p>javascript,node-js</p>");
             assert.notInclude(out.compiled, "<p>Post 2</p>\n<p>Post 2</p>");
+        });
+    });
+    it("can re-render category list when it's updated", function(done) {
+
+        var postItem  = crossbow.addPost("_posts/post1.md", post1);
+        var postItem2 = crossbow.addPost("_posts/post2.md", post2);
+
+        crossbow.compileOne(postItem, {siteConfig: {categories: ["js", "ruby"]} }, function (err, out) {
+
+             crossbow.addPost("_posts/post1.md", post1_alt);
+
+             crossbow.compileOne("_posts/post1.md", {}, function (err, out) {
+                 assert.include(out.compiled, "<p>javascript</p>");
+                 done();
+             });
         });
     });
 });
