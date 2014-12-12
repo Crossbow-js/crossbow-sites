bf68fb8f6f0f20ea58e4bd4d1a43e04ac0f8c49c
Shane Osbourne <shane.osbourne8@gmail.com>
2014-12-12 23:55:03 +0000

    Add bench

---
+++ example-bench.js
@@ -0,0 +1,15 @@
+var crossbow    = require("./plugins/stream");
+//var through     = require("through2");
+var fs          = require("vinyl-fs");
+//var rimraf      = require("rimraf").sync;
+var outpath     = "./_bench-out";
+
+//rimraf(outpath);
+
+fs.src([
+    "_bench/*.html"
+])
+    .pipe(crossbow({cwd: "_bench"}))
+    .pipe(fs.dest(outpath));
+
+
