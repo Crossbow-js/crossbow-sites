268c32b1723cac148d7bd75ed621bb06c6434b37
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-09 07:51:10 +0000

    Use step/fn for rendering

--- test/new/process/addPage.js
+++ test/new/process/addPage.js
@@ -3,7 +3,7 @@ var assert    = require("chai").assert;
 var multiline = require("multiline");
 var sinon     = require("sinon");
 var fs        = require("fs");
-var crossbow = require("../../../index");
+var crossbow  = require("../../../index");
 
 describe("Adding a page", function() {
 
