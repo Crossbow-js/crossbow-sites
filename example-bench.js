f0caf5302a52595fcb0ca0028eee70a646be7d33
Shane Osbourne <shane.osbourne8@gmail.com>
2015-03-02 07:25:50 +0000

    Ignore bench example

--- example-bench.js
+++ example-bench.js
@@ -1,5 +1,4 @@
 var crossbow    = require("./plugins/stream");
-//var through     = require("through2");
 var fs          = require("vinyl-fs");
 var rimraf      = require("rimraf").sync;
 var outpath     = "./_bench-out";
@@ -13,8 +12,9 @@ var site        = require("./").builder({
 rimraf(outpath);
 
 console.time("bench");
+
 fs.src([
-    "_bench/*.html"
+    "_bench/*.md"
     //"_bench/1-file.html"
 ])
     .pipe(crossbow({builder: site}))
